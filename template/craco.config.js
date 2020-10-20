35f320db37278d997439d26788121d5d48f16915
gscottqueen <scott.queen@bixal.com>
2020-10-20 15:55:59 -0400

    updated env config to handle craco env build variables

--- template/craco.config.js
+++ template/craco.config.js
@@ -1,10 +1,10 @@
-const appName = process.env.REACT_APP_NAME
+require('dotenv').config() // we use dotenv because we can't access react's from here on build
 
 module.exports = {
   webpack: {
     configure: {
       output: {
-        filename: 'static/js/'+ appName + '.min.js'
+        filename: 'static/js/'+ process.env.REACT_APP_NAME + '.min.js'
       },
       optimization: {
         runtimeChunk: false,
@@ -21,7 +21,7 @@ module.exports = {
       plugin: {
         overrideWebpackConfig: ({ webpackConfig }) => {
           // we overide the filename for our output files
-          webpackConfig.plugins[5].options.filename = 'static/css/'+ appName + '.min.css';
+          webpackConfig.plugins[5].options.filename = 'static/css/'+ process.env.REACT_APP_NAME + '.min.css';
           webpackConfig.module.rules[2].oneOf[0].options.name = 'static/media/[name].[ext]'
           webpackConfig.module.rules[2].oneOf[7].options.name = 'static/media/[name].[ext]'
           return webpackConfig;
--- template/src/index.js
+++ template/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-const appName = process.env.REACT_APP_NAME
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById(appName)
+  document.getElementById(process.env.REACT_APP_NAME)
 );
 
 // If you want your app to work offline and load faster, you can change
